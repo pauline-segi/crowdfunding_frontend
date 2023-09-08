78c77a57bd5890fea204677ce15ee95dbc167190
Pauline Segi <p.segi@outlook.com>
2023-09-09 09:54:35 +1000

    First bunch of React files for Crowdfunding project

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import "./ProjectCard.css";
+
+
+function ProjectCard(props) {
+    const { projectData } = props;
+    const projectLink = `project/${projectData.id}`;
+    
+    return (
+        <div>
+            <Link to={projectLink}>
+                <img src={projectData.image} />
+                <h3>{projectData.title}</h3>
+            </Link>
+        </div>
+    );
+}
+
+export default ProjectCard;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import useProjects from "../hooks/use-projects";
+import ProjectCard from "../components/ProjectCard";
+import "./HomePage.css";
+
+function HomePage() {
+    const { projects } = useProjects();
+    
+    return (
+        <div id="project-list">
+            {projects.map((projectData, key) => {
+                return <ProjectCard key={key} projectData={projectData} />;
+            })}
+        </div>
+    );
+}
+
+export default HomePage;
