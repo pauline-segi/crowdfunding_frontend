66f4bfc0916e63b1c18b9e1838c70053adee1490
Pauline Segi <p.segi@outlook.com>
2023-09-09 12:33:48 +1000

    created a login form and Post request

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,8 +3,16 @@ import ProjectCard from "../components/ProjectCard";
 import "./HomePage.css";
 
 function HomePage() {
-    const { projects } = useProjects();
-    
+    const { projects, isLoading, error } = useProjects();
+
+    if (isLoading) {
+        return (<p>loading...</p>)
+    }
+
+    if (error) {
+        return (<p>{error.message}</p>)
+    }
+
     return (
         <div id="project-list">
             {projects.map((projectData, key) => {
