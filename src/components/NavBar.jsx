65aad9cdaef893d2aebf06ea254cd3d8f23a9fdd
Pauline Segi <p.segi@outlook.com>
2023-09-24 20:28:17 +1000

    outlet

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -69,6 +69,7 @@ function NavBar() {
                             </Link> 
                         )}
                     </nav>
+                    <Outlet />
                 </div>
             )}
 
@@ -104,6 +105,7 @@ function NavBar() {
                             </Link>
                         )}
                     </nav>
+                    <Outlet />
                 </div>
             )}
 
